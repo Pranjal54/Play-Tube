d1115be9669e3715bf4471be433cc4c0308e32f3
Pranjal54 <mishrapranjal0402@gmail.com>
2025-03-26 17:19:10 +0530

    commit

--- PlayTube-Frontend-main/src/helpers/axios.helper.js
+++ PlayTube-Frontend-main/src/helpers/axios.helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
 
-const baseURL = "https://playtube-sq4g.onrender.com/api/v1";
+const baseURL = "https://playtube-bjm3.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
