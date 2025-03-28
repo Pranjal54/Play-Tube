9eee1d023b2c9f58b1505e29e4035bc18c1b78af
Pranjal54 <mishrapranjal0402@gmail.com>
2025-03-28 11:18:12 +0530

    commit

--- PlayTube-Frontend-main/src/pages/Support.jsx
+++ PlayTube-Frontend-main/src/pages/Support.jsx
@@ -22,37 +22,25 @@ function Support() {
                 <div className="h-full">
                   <h2 className="text-lg font-bold my-0">LikedIn</h2>
                   <a
-                    href="https://www.linkedin.com/in/siddhant-tomar-66b354227/"
+                    href="https://www.linkedin.com/in/pranjal-mishra-78202922b/"
                     target="_blank"
                     className="text-blue-500 text-sm hover:text-blue-400 "
                   >
-                    /siddhant-tomar
+                    /pranjal-mishra
                   </a>
                 </div>
               </li>
-              <li className="flex items-center">
-                <span className="w-9 h-9 rounded-full mr-4">{icons.link}</span>
-                <div className="h-full">
-                  <h2 className="text-lg font-bold my-0">LikedIn</h2>
-                  <a
-                    href="https://www.linkedin.com/in/parth-bhatia-051538297/"
-                    target="_blank"
-                    className="text-blue-500 text-sm hover:text-blue-400 "
-                  >
-                    /parth-bhatia
-                  </a>
-                </div>
-              </li>
+              
               <li className="flex items-center">
                 <span className="w-9 h-9 rounded-full mr-4">{icons.link}</span>
                 <div className="h-full">
                   <h2 className="text-lg font-bold my-0">GitHub</h2>
                   <a
-                    href="https://github.com/Parthbhatia2003"
+                    href="https://github.com/Pranjal54"
                     target="_blank"
                     className="text-blue-500 text-sm hover:text-blue-400 "
                   >
-                    /Parth-bhatia
+                    /pranjal54
                   </a>
                 </div>
               </li>
